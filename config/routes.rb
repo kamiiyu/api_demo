1fd0a309e747550bd6d96e126f734029ce83a4a5
kamiiyu <spicekamiiyu@gmail.com>
2016-02-25 07:39:13 +0800

    add v1, v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :articles, except: [:new, :edit]
-  resources :authors, except: [:new, :edit]
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :articles, except: [:new, :edit]
+      resources :authors, except: [:new, :edit]
+    end
+    namespace :v2 do
+      resources :articles, except: [:new, :edit]
+      resources :authors, except: [:new, :edit]
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
