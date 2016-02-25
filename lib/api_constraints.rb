49142752d47c827ee211c39b5f5b5d709fe98a98
kamiiyu <spicekamiiyu@gmail.com>
2016-02-25 13:05:44 +0800

    add flow chart

--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -12,6 +12,7 @@ class ApiConstraints
   protected
   def authenticity_check(req)
     # 2a7f629d41e44325413f8966af026ec978eca044
+    # appid appsecret
     _client1_token = Digest::SHA1.hexdigest 'client1_api_demo_20160225'
     req.headers['Accept'].include?("application/vnd.api_demo.v#{@version}.t#{_client1_token}")
   end
